be819b50fcc78e7da08bbeeae8a17ef648d48bef
Abderrahman DAKIR ALLAH <dakirallah.ar@gmail.com>
2022-11-26 00:36:31 +0100

    syncing db tables

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { db } from "./db/dbConnection.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json({status : "Working"});
+    res.status(200).json({ status: "Working" });
 });
 
+db.sync({ force: true });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {console.log("Listening on port", port);});
+app.listen(port, () => { console.log("Listening on port", port); });
