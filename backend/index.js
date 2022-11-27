6ee5f3adce344d6079d9a08ebd79fdbac5e1c716
Abderrahman DAKIR ALLAH <dakirallah.ar@gmail.com>
2022-11-27 23:20:50 +0100

    commented syncing db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { newEntitiesRoute, editEntitiesRoute, getEntitiesRoute } from "./routes/routes.env.js"
+// import { db } from "./db/dbConnection.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.status(200).json({ status: "Working" });
 });
+
+// db.sync();
 
 app.use("/new", newEntitiesRoute);
 app.use("/edit", editEntitiesRoute);
